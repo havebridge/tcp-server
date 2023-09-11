3dcf1e8b9bb76eba75661650b0a417720ce28d9d
havebridge <meyl0r@yandex.com>
2023-09-11 22:40:55 +0300

    thread pool using futures with packaged task, mb switch to smthelse

---
+++ ThreadPool.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <queue>
+#include <vector>
+#include <functional>
+
+#include <thread>
+#include <future>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+namespace Core
+{
+	class ThreadPool
+	{
+	private:
+		std::vector<std::thread> threads;
+		std::queue<std::function<void()>> jobs;
+		std::mutex job_mutex;
+		std::condition_variable cv;
+		std::atomic<bool> is_terminated = false;
+
+	private:
+		void ThreadLoop()
+		{
+			std::function<void()> job;
+
+			while (!is_terminated)
+			{
+				{
+					std::unique_lock<std::mutex> job_lock(job_mutex);
+
+					cv.wait(job_lock, [this] {
+						return !jobs.empty() || is_terminated;
+						});
+
+					if (is_terminated)
+					{
+						return;
+					}
+
+					job = std::move(jobs.front());
+					jobs.pop();
+				}
+				job();
+			}
+		}
+
+		void PoolSetup()
+		{
+			threads.clear();
+
+			for (int i = 0; i < std::thread::hardware_concurrency(); ++i)
+			{
+				threads.emplace_back(std::thread(&ThreadPool::ThreadLoop, this));
+			}
+		}
+
+	public:
+		ThreadPool()
+		{
+			PoolSetup();
+		}
+
+		~ThreadPool()
+		{
+			for (auto& thread : threads)
+			{
+				if (thread.joinable())
+				{
+					thread.join();
+				}
+			}
+		}
+
+	public:
+		void Join()
+		{
+			for (auto& thread : threads)
+			{
+				if (thread.joinable())
+				{
+					thread.join();
+				}
+			}
+		}
+
+		template<typename F, typename... Args>
+		auto QueueJob(F&& f, Args&&... args) -> std::future<decltype(f(args...))>
+		{
+			//TODO(): use c++23 by adding move_only_function
+
+			auto func = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
+			auto task_ptr = std::make_shared<std::packaged_task<decltype(f(args...))()>>(func);
+			auto wrapper = [task_ptr]() {(*task_ptr)(); };
+
+			{
+				std::lock_guard<std::mutex> job_lock(job_mutex);
+				jobs.push(wrapper);
+				cv.notify_one();
+			}
+
+			return task_ptr->get_future();
+		}
+
+		void DropJobs()
+		{
+			is_terminated = true;
+			Join();
+			is_terminated = false;
+			std::queue<std::function<void()>> empty;
+			std::swap(jobs, empty);
+			PoolSetup();
+		}
+	};
+}
--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,16 +1,30 @@
 #include "../include/client.h"
 
+#include "../../ThreadPool.h"
+
+void job(void)
+{
+	std::cout << "55" << '\n';
+}
+
+
 int main(int argc, char** argv)
 {
 	(void)argc;
 	(void)argv;
 
-	Net::Client client;
+	//Net::Client client;
 
-	if (client.Connect("127.0.0.1", 8888))
+	/*if (client.Connect("127.0.0.1", 8888))
 	{
 		std::cout << "Good\n";
-	}
+	}*/
+
+	Core::ThreadPool thread_pool;
+
+	thread_pool.QueueJob(std::bind(&job));
+
+	thread_pool.Join();
 
 	return 0;
 }
